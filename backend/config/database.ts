fd8b2beb37fb5b19cb469c83a5d0e60683ee6e4a
AnasSaifeee <as453196@gmail.com>
2025-12-28 18:46:17 +0530

    added database module

---
+++ backend/config/database.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+async function InitializeDBConnection(): Promise<void>{
+    try{
+        await mongoose.connect('mongodb://localhost:27017/collabeditor', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        } as mongoose.ConnectOptions);
+    }catch(err){
+        console.error('MongoDB connection error:', err);
+        process.exit(1);
+    }
+}
+
+export default InitializeDBConnection
