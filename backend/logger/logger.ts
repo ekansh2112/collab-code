dc2705f768d3ad0be6c7e480d32cf3103d68d655
AnasSaifeee <as453196@gmail.com>
2025-12-28 21:01:00 +0530

    commenting logfile creation for now

--- backend/logger/logger.ts
+++ backend/logger/logger.ts
@@ -12,7 +12,7 @@ import DailyRotateFile from 'winston-daily-rotate-file';
 // });
 
 
-const logger = createLogger({
+const logger  = createLogger({
   level: 'info', 
   format: format.combine(
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
@@ -30,8 +30,8 @@ const logger = createLogger({
       )
     }),
     // dailyRotateTransport,
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' })
+    // new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    // new transports.File({ filename: 'logs/combined.log' })
   ],
   exitOnError: false,
 });
