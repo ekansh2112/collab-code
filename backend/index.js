54a352b13aaa6a8c23096abc1bc5859e153e68c7
Ekansh Gupta <ekanshgupta@WINDELLAP-1173.local>
2025-12-28 15:41:59 +0530

    fixes for yjs

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,11 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 3001 });
 
 wss.on("connection", (ws, request) => {
+  // const url = new URL(request.url,"http://localhost:3001");
   const url = new URL(request.url,"https://collab-code-uj27.onrender.com");
   const roomId = url.searchParams.get("room");
-  console.log("roomId: %s", roomId);
   ws.roomId = roomId;
   ws.on("message", (data) => {
-    // Broadcast to all other connected clients
-    console.log("received: %s", data);
     wss.clients.forEach((client) => {
       if(client !== ws && client.readyState === 1 && roomId && client.roomId === roomId) {
         client.send(data)
--- frontend/src/components/EditorPanel.tsx
+++ frontend/src/components/EditorPanel.tsx
@@ -2,28 +2,50 @@ import { useEffect, useRef } from "react";
 import Editor from "@monaco-editor/react";
 import * as Y from 'yjs'
 import { MonacoBinding } from 'y-monaco'
-import { WebrtcProvider } from 'y-webrtc'
-// import { WebsocketProvider } from 'y-websocket'
+import { WebsocketProvider } from 'y-websocket'
 
 export default function EditorPanel({ roomId }: { roomId: string }) {
   const ydoc = useRef<Y.Doc>(null);
-  const provider = useRef<WebrtcProvider>(null);
-//   const provider = useRef<WebsocketProvider>(null);
+  const provider = useRef<WebsocketProvider>(null);
   const bindingRef = useRef<MonacoBinding>(null);
   const type = useRef<Y.Text>(null);
 
   useEffect(() => {
     ydoc.current = new Y.Doc();
-    provider.current = new WebrtcProvider(roomId, ydoc.current!);
-    // provider.current = new WebsocketProvider("wss://collab-code-uj27.onrender.com", roomId, ydoc.current!);
-    type.current = ydoc.current!.getText("togethercoding")
+    // provider.current = new WebsocketProvider("ws://localhost:3001?room=" + roomId, roomId, ydoc.current!);
+    provider.current = new WebsocketProvider("wss://collab-code-uj27.onrender.com?room=" + roomId, roomId, ydoc.current!);
+    type.current = ydoc.current!.getText("togethercoding123")
+    
+    provider.current.on("status", (event) => {
+        console.log("WebSocket status:", event.status);
+      });
+    
     return () => {
-      bindingRef.current?.destroy()
-      provider.current?.disconnect();
-      provider?.current?.destroy()
-      ydoc.current?.destroy()
+        console.log("Cleanup WebSocket + Ydoc connection");
+        bindingRef.current?.destroy();
+        provider?.current?.destroy();
+        ydoc.current?.destroy();
     }
   }, [roomId]);
+
+  useEffect(() => {
+    if (!ydoc.current) return;
+    const update = Y.encodeStateAsUpdate(ydoc.current!);
+    //POST call to backend to save the update
+    console.log("update-------> ", update);
+  }, [roomId]);
+
+  //GET call to update from backend
+//   useEffect(() => {
+//     fetch(`http://localhost:3001/load?roomId=${roomId}`)
+//       .then(res => res.json())
+//       .then(data => {
+//         if (data.encodedUpdate) {
+//           const update = new Uint8Array(data.encodedUpdate);
+//           Y.applyUpdate(ydoc.current, update);
+//         }
+//       });
+//   }, [roomId]);
 
   return (
     <Editor
